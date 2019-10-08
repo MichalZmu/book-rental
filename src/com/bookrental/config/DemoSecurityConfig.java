bedc8dda40a0ac1fe9e9821aa5902c0b678c4e52
MichalZmu <michal.zmuda.mail@gmail.com>
2019-10-08 09:01:15 +0200

    Add new config file

---
+++ src/com/bookrental/config/DemoSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.bookrental.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource securityDataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// add our users for in memory authentication
+		
+		/*UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("paul").password("test123").roles("MANAGER"))
+			.withUser(users.username("michael").password("test123").roles("ADMIN"));*/
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests().antMatchers("/").permitAll()
+		.antMatchers("/employees").hasRole("EMPLOYEE")
+		.antMatchers("/leaders/**").hasRole("MANAGER")
+		.and().formLogin().loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll()
+		.and().logout().logoutSuccessUrl("/").permitAll()
+		.and().exceptionHandling().accessDeniedPage("/accessDenied");
+	
+	}
+	
+	
+
+}
